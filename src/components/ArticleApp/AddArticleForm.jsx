26f73ac7f6b06e367f24d252a788366cadb2d790
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-05-03 14:53:29 +0300

    RHF

--- src/components/ArticleApp/AddArticleForm.jsx
+++ src/components/ArticleApp/AddArticleForm.jsx
@@ -1,36 +1,37 @@
-import { useState } from "react";
 import { Button } from "../Button/Button";
 import s from "./ArticleApp.module.css";
-export const AddArticleForm = ({ handleAddArticle }) => { 
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-    const [author, setAuthor] = useState("");
-    
-  const handleSubmit = () => {
-    handleAddArticle({ title, body, author });
-    };
-    
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as Yup from 'yup'
+import { InputField } from "./InputField";
+
+const schemaValidate = Yup.object().shape({
+  title: Yup.string().min(3, 'Too short'),
+  body: Yup.string().max(20, 'Too long!'),
+  author: Yup.string().required('Required field')
+})
+
+export const AddArticleForm = ({ handleAddArticle }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schemaValidate),
+  });
+  const onSubmit = (data) => {
+    handleAddArticle(data);
+  };
+
   return (
-    <div className={s.form}>
-      <input
-        className={s.input}
-        type="text"
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Enter title"
-      />
-      <input
-        className={s.input}
-        type="text"
-        onChange={(e) => setBody(e.target.value)}
-        placeholder="Enter body"
-      />
-      <input
-        className={s.input}
-        type="text"
-        onChange={(e) => setAuthor(e.target.value)}
-        placeholder="Enter author"
-      />
-      <Button onClick={handleSubmit}>Add article</Button>
+    <div>
+      <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+        <InputField register={register} errors={errors} name='title' placeholder='Enter title' />
+        <InputField register={register} errors={errors} name='body' placeholder='Enter body' />
+        <InputField register={register} errors={errors} name='author' placeholder='Enter author' />
+        
+        <Button onClick={handleSubmit}>Add article</Button>
+      </form>
     </div>
   );
 };
