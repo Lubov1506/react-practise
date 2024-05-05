f55977ebd69bdc7213a746ff327cd5dfe58e6b90
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-05-05 18:37:05 +0300

    fix something

--- src/components/FormRHF/FormRHF.jsx
+++ src/components/FormRHF/FormRHF.jsx
@@ -3,9 +3,11 @@ import s from "./FormRHF.module.css";
 import { Input } from "./Input";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+
 const valSchema = Yup.object().shape({
-  username: Yup.string().required("Required"),
-  surname: Yup.string().required("Required surname"),
+  username: Yup.string().required("Required").min(3, 'Too short').max(32, 'Too long'),
+    surname: Yup.string().required("Required surname").min(3, 'Too short').max(32, 'Too long'),
+  email:Yup.string().matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Not valid email')
 });
 
 export const FormRHF = () => {
@@ -15,16 +17,18 @@ export const FormRHF = () => {
     formState: { errors },
     handleSubmit,
   } = useForm({
+      mode: 'onSubmit',
+      reValidateMode: 'onBlur',
     resolver: yupResolver(valSchema),
   });
   const onSubmit = (data) => {
     console.log(data);
-    // reset();
+    reset();
   };
   const fields = [
     { name: "username", type: "text" },
     { name: "surname", type: "text" },
-    { name: "email", type: "email" },
+    { name: "email", type: "text" },
     { name: "password", type: "password" },
   ];
   return (
