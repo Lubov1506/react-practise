fe45239d1670162852147624de2a7c5c8552563f
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-05-17 09:05:22 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,11 @@
 import { useDispatch } from "react-redux";
-import { NavLink, Outlet } from "react-router-dom";
-import { logout } from "../redux/authSlice";
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const Layout = () => {
-  const dispatch = useDispatch();
   return (
     <div className="flex flex-col min-h-screen">
-
-
-      <Navbar/>
+      <Navbar />
       <main className="flex-grow">
         <Outlet />
       </main>
